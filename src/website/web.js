656617701013403e94f247325c19fd7708db9b04
Vinicius de Moraes Nascimento <vinicci.exe@gmail.com>
2022-01-23 10:55:18 +0000

    up

--- src/website/web.js
+++ src/website/web.js
@@ -7,7 +7,7 @@ import "../css/web.css"
 // import {T, useSetLanguage, useCurrentLanguage } from "@tolgee/react"
 let loader = []
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
 const cameraGroup = new THREE.Group()
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -28,12 +28,35 @@ let currentSection = 0
 function Web() {
   useEffect(() => {
     
-    camera.position.z = 5;
-    camera.rotation.z = 1; 
+    let tl = window.addEventListener('scroll', () =>
+    {
+        scrollY = window.scrollY
+        const newSection = Math.round(scrollY / window.innerHeight)
     
-  
+        if(newSection != currentSection)
+        {
+            currentSection = newSection
+    
+            gsap.to(
+                sectionMeshes[currentSection].rotation,
+                {
+                    duration: 1.5,
+                    ease: 'power2.inOut',
+                    x: '+=6',
+                    y: '+=3',
+                    z: '+=1.5'
+                }
+            )
+        }
+        
+        return scrollY
+    })
+
+    
+
+
       
-      scene.add( light );
+      // scene.add( light );
 
       // const setEnvironment=()=>{
         
@@ -59,19 +82,19 @@ function Web() {
         // }
   
   
-      loader[0].load("/models/free__house_low-poly_isometric/scene.gltf", (gltf) => {
+      loader[0].load("/models/space_exploration_wlp_series_8/scene.gltf", (gltf) => { //isometric_rooms
           let model = gltf.scene
-          model.scale.set(.1, .1, .1)
+         // model.scale.set(.5, .5, .5)
   
-          gsap.to(camera.position, {
-              z: 1,
-              duration: 1,
-              ease: "back.out(1.7)"
-          })
-          gsap.to(camera.rotation, {
-              z: 0,
-              duration: 1
-          })
+          // gsap.to(camera.position, {
+          //     z: 1,
+          //     duration: 1,
+          //     ease: "back.out(1.7)"
+          // })
+          // gsap.to(camera.rotation, {
+          //     z: 0,
+          //     duration: 1
+          // })
   
           gsap.to(model.rotation, {
               x: 1,
@@ -93,9 +116,14 @@ function Web() {
           gsap.to(model.position, {
               delay: 1,
               duration: 1,
-              x: .35,
-              y: .3
+              x: 2,
+              y: 0
           })
+          gsap.to(model.rotation, {
+            y: Math.PI * 1.75,
+            duration: 5,
+            delay: 1
+        })
   
   
   
@@ -103,35 +131,116 @@ function Web() {
       })
 
 
-    const gradientTexture = textureLoader.load('/images/pizza.png')
+      loader[0].load("/models/drone/scene.gltf", (gltf) => { //isometric_rooms
+        let robot = gltf.scene
+        robot.scale.set(5, 5, 5)
+        // robot.position.set(0, 0, 0)
+       var sectionDuration = 1;
+       let tau = Math.PI * 2;
+
+      // let tl = new gsap.timeline(
+      //   {
+      //     onUpdate: scene.render,
+      //     scrollTrigger: {
+      //       trigger: ".section",
+      //       scrub: true,
+      //       start: "top top",
+      //       end: "bottom bottom"
+            
+      //     },
+      //     defaults: {duration: sectionDuration, ease: 'power2.inOut'}
+          
+      //   });
+
+        // console.log('t1',tl)
+        
+        let delay = 0;
+        
+        //tl.to('.scroll-cta', {duration: 0.25, opacity: 0}, delay)
+        tl.to(robot.position, {x: -1, ease: 'power1.in'}, delay)
+        
+        delay += sectionDuration;
+        
+        tl.to(robot.rotation, {x: tau * .25, y: 0, z: -tau * 0.05, ease: 'power1.inOut'}, delay)
+        tl.to(robot.position, {x: -4, y: 0, z: -60, ease: 'power1.inOut'}, delay)
+        
+        delay += sectionDuration;
+        
+        tl.to(robot.rotation, {x: tau * .25, y: 0,  z: tau * 0.05, ease: 'power3.inOut'}, delay)
+        tl.to(robot.position, {x: 4, y: 0, z: -60, ease: 'power2.inOut'}, delay)
+        
+        delay += sectionDuration;
+        
+        tl.to(robot.rotation, {x: tau * .2, y: 0, z: -tau * 0.1, ease: 'power3.inOut'}, delay)
+        tl.to(robot.position, {x: -4, y: 0, z: -30, ease: 'power2.inOut'}, delay)
+        
+        delay += sectionDuration;
+        
+        tl.to(robot.rotation, { x: 0, z: 0, y: tau * .25}, delay)
+        tl.to(robot.position, { x: 0, y: -10, z: 50}, delay)
+        
+        delay += sectionDuration;
+        delay += sectionDuration;
+        
+        tl.to(robot.rotation, {x: tau * 0.25, y: tau *.5, z: 0, ease:'power4.inOut'}, delay)
+        tl.to(robot.position, {z: 3, ease:'power4.inOut'}, delay)
+        
+        delay += sectionDuration;
+        
+        tl.to(robot.rotation, {x: tau * 0.25, y: tau *.5, z: 0, ease:'power4.inOut'}, delay)
+        tl.to(robot.position, {z: 6, x: 30, ease:'power4.inOut'}, delay)
+        
+        delay += sectionDuration;
+        
+        tl.to(robot.rotation, {x: tau * 0.35, y: tau *.75, z: tau * 0.6, ease:'power4.inOut'}, delay)
+        tl.to(robot.position, {z: 10, x: 20, y: 0, ease:'power4.inOut'}, delay)
+        
+        delay += sectionDuration;
+        
+        tl.to(robot.rotation, {x: tau * 0.15, y: tau *.85, z: -tau * 0, ease: 'power1.in'}, delay)
+        tl.to(robot.position, {z: -15, x: 0, y: 0, ease: 'power1.inOut'}, delay)
+        
+        delay += sectionDuration;
+        
+        tl.to(robot.rotation, {duration: sectionDuration, x: -tau * 0.05, y: tau, z: -tau * 0.1, ease: 'none'}, delay)
+        tl.to(robot.position, {duration: sectionDuration, x: 0, y: 3, z: 3.2, ease: 'power1.in'}, delay)
+        
+        tl.to(scene.directionalLight.position, {duration: sectionDuration, x: 0, y: 0, z: 0}, delay)
+
+        scene.add(robot)
+      })
+
+
+    const gradientTexture = textureLoader.load('/images/5.jpg')
     gradientTexture.magFilter = THREE.NearestFilter
     
     // Material
     const material = new THREE.MeshToonMaterial({
-        color: 0xffeded,//parameters.materialColor,
-        gradientMap: gradientTexture
+        color: '#ffff00'
+        ////parameters.materialColor,
+        ,gradientMap: gradientTexture
     })
 
     const mesh1 = new THREE.Mesh(
-      new THREE.TorusGeometry(1, 0.4, 16, 60),
+      new THREE.TorusKnotGeometry(0.5, 0.1, 64, 32),
       material
     )
     const mesh2 = new THREE.Mesh(
-        new THREE.ConeGeometry(1, 2, 32),
+        new THREE.TorusKnotGeometry(0.5, 0.1, 64, 32),
         material
     )
     const mesh3 = new THREE.Mesh(
-        new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+        new THREE.TorusKnotGeometry(0.5, 0.1, 64, 32),
         material
     )
     
-    mesh1.position.x = 2
-    mesh2.position.x = - 2
-    mesh3.position.x = 2
+    mesh1.position.x = -2
+    mesh2.position.x =  2
+    mesh3.position.x = -2
     
-    mesh1.position.y = - objectsDistance * 0
-    mesh2.position.y = - objectsDistance * 1
-    mesh3.position.y = - objectsDistance * 2
+    mesh1.position.y = - objectsDistance * 2
+    mesh2.position.y = - objectsDistance * 3
+    mesh3.position.y = - objectsDistance * 4
     
     scene.add(mesh1, mesh2, mesh3)
     
@@ -167,32 +276,10 @@ function Web() {
     scene.add(particles)
 
     scene.add(cameraGroup)
-    camera.position.z = 6
+    camera.position.z = 4
     cameraGroup.add(camera)
 
 
-
-    window.addEventListener('scroll', () =>
-    {
-        scrollY = window.scrollY
-        const newSection = Math.round(scrollY / window.innerHeight)
-    
-        if(newSection != currentSection)
-        {
-            currentSection = newSection
-    
-            gsap.to(
-                sectionMeshes[currentSection].rotation,
-                {
-                    duration: 1.5,
-                    ease: 'power2.inOut',
-                    x: '+=6',
-                    y: '+=3',
-                    z: '+=1.5'
-                }
-            )
-        }
-    })
 
     cursor.x = 0
     cursor.y = 0
@@ -223,10 +310,12 @@ function Web() {
 
     <div className="Web">
 
+      
+
     <div className="App">
     <div className="hero">
       <h1 className="title">Gaia Space Matrix</h1>
-      <p className="text">Cooworking</p>
+      <p className="text">COOWORKING</p>
       {/* <p>
         <button className="btn" onClick={() => setLanguage("en")} style={{background: getLanguage() === "en" ? "#ffba02" : "#a7a7a7"}}>English</button>
         <button className="btn" onClick={() => setLanguage("ru")} style={{background: getLanguage() === "ru" ? "#ffba02" : "#a7a7a7"}}>Russian</button>
@@ -234,22 +323,22 @@ function Web() {
     </div>
 
     <div className="pricing">
-      <h1>our_packages</h1>
+      <h1>our packages</h1>
       <div className="pricing-table">
         <div className="pricing-card">
           <p className="title">Private Room</p>
-          <p className="info">card_text</p>
-          <button className="btn">text_buy</button>
+          <p className="info">Rent a room for your business </p>
+          <button className="btn">rent</button>
         </div>
         <div className="pricing-card">
           <p className="title">Shared Space</p>
-          <p className="info">card_text</p>
-          <button className="btn">text_buy</button>
+          <p className="info">Access common environments with privileged services </p>
+          <button className="btn">rent</button>
         </div>
         <div className="pricing-card">
           <p className="title">Collective Events</p>
           <p className="info">card_text</p>
-          <button className="btn">text_buy</button>
+          <button className="btn">rent</button>
         </div>
       </div>
     </div>
@@ -267,9 +356,12 @@ function Web() {
           <h2>Contact me</h2>
       </section>
 
+
+     
+
       </div>
 
-    
+
 
   );
 }
